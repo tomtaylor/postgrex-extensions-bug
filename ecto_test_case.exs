801ef9e81d4a6f3b97c19602528059f0b0299a90
Tom Taylor <tom@tomtaylor.co.uk>
2022-09-14 22:36:30 +0100

    Use ALTER USER to set working search path

--- ecto_test_case.exs
+++ ecto_test_case.exs
@@ -3,23 +3,26 @@ Mix.install([
   {:postgrex, "0.16.4"}
 ])
 
-Application.put_env(:app, Repo, database: "postgres_extensions_bug")
-
-query_args = ["SET search_path TO public,heroku_ext", []]
-
 defmodule Repo do
   use Ecto.Repo,
     adapter: Ecto.Adapters.Postgres,
-    otp_app: :app,
-    after_connect: {Postgrex, :query!, query_args}
+    otp_app: :app
 end
 
 defmodule Migration0 do
   use Ecto.Migration
 
   def up do
+    username =
+      System.cmd("whoami", [])
+      |> case do
+        {username, 0} -> String.trim(username)
+        _ -> raise "Could not get system username"
+      end
+
     execute("CREATE SCHEMA heroku_ext")
     execute("CREATE EXTENSION citext WITH SCHEMA heroku_ext")
+    execute("ALTER USER #{username} SET search_path = public, heroku_ext")
   end
 end
 
@@ -27,8 +30,6 @@ defmodule Migration1 do
   use Ecto.Migration
 
   def up do
-    execute("SET search_path = public,heroku_ext")
-
     create table(:users) do
       add(:email, :citext, null: false)
     end
@@ -45,6 +46,8 @@ end
 
 defmodule Main do
   def main() do
+    Application.put_env(:app, Repo, database: "postgres_extensions_bug")
+
     children = [
       Repo
     ]
@@ -62,7 +65,8 @@ defmodule Main do
     %User{email: "foo@bar.com"}
     |> Repo.insert!()
 
-    IO.inspect(Repo.all(User))
+    Repo.all(User)
+    |> IO.inspect()
   end
 end
 
